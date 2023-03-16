f18851133e3d7a26607dd6f1f366248c962c3dd7
Jethro Yu <comet.jc@gmail.com>
2023-03-17 00:20:12 +0800

    Use zerossl CA. Fix full chain certificate
    Use `Comodo AAA Certificate Services` for zerossl.
    
    The correct foramt should be 
    1. full cahin certificates with root CA
    2. private key
    3. full chain intermediate certificates with root CA
    thus vcenter can provide full chain certificates in SSL response.

--- auto-updater.sh
+++ auto-updater.sh
@@ -62,12 +62,45 @@ fi
 # We need to update this machine with the new certificate.
 KEY=/root/.acme.sh/$CERTNAME/$CERTNAME.key
 CHAIN=/root/.acme.sh/$CERTNAME/fullchain.cer
-CHAIN_WITH_ROOT=/root/.acme.sh/$CERTNAME/fullchainwithroot.cer
+CA=/root/.acme.sh/$CERTNAME/ca.cer
+
+# Comodo AAA Certificate Services
+ROOT=/root/.acme.sh/$CERTNAME/root.cer
+cat > ${ROOT} <<EOF
+-----BEGIN CERTIFICATE-----
+MIIEMjCCAxqgAwIBAgIBATANBgkqhkiG9w0BAQUFADB7MQswCQYDVQQGEwJHQjEb
+MBkGA1UECAwSR3JlYXRlciBNYW5jaGVzdGVyMRAwDgYDVQQHDAdTYWxmb3JkMRow
+GAYDVQQKDBFDb21vZG8gQ0EgTGltaXRlZDEhMB8GA1UEAwwYQUFBIENlcnRpZmlj
+YXRlIFNlcnZpY2VzMB4XDTA0MDEwMTAwMDAwMFoXDTI4MTIzMTIzNTk1OVowezEL
+MAkGA1UEBhMCR0IxGzAZBgNVBAgMEkdyZWF0ZXIgTWFuY2hlc3RlcjEQMA4GA1UE
+BwwHU2FsZm9yZDEaMBgGA1UECgwRQ29tb2RvIENBIExpbWl0ZWQxITAfBgNVBAMM
+GEFBQSBDZXJ0aWZpY2F0ZSBTZXJ2aWNlczCCASIwDQYJKoZIhvcNAQEBBQADggEP
+ADCCAQoCggEBAL5AnfRu4ep2hxxNRUSOvkbIgwadwSr+GB+O5AL686tdUIoWMQua
+BtDFcCLNSS1UY8y2bmhGC1Pqy0wkwLxyTurxFa70VJoSCsN6sjNg4tqJVfMiWPPe
+3M/vg4aijJRPn2jymJBGhCfHdr/jzDUsi14HZGWCwEiwqJH5YZ92IFCokcdmtet4
+YgNW8IoaE+oxox6gmf049vYnMlhvB/VruPsUK6+3qszWY19zjNoFmag4qMsXeDZR
+rOme9Hg6jc8P2ULimAyrL58OAd7vn5lJ8S3frHRNG5i1R8XlKdH5kBjHYpy+g8cm
+ez6KJcfA3Z3mNWgQIJ2P2N7Sw4ScDV7oL8kCAwEAAaOBwDCBvTAdBgNVHQ4EFgQU
+oBEKIz6W8Qfs4q8p74Klf9AwpLQwDgYDVR0PAQH/BAQDAgEGMA8GA1UdEwEB/wQF
+MAMBAf8wewYDVR0fBHQwcjA4oDagNIYyaHR0cDovL2NybC5jb21vZG9jYS5jb20v
+QUFBQ2VydGlmaWNhdGVTZXJ2aWNlcy5jcmwwNqA0oDKGMGh0dHA6Ly9jcmwuY29t
+b2RvLm5ldC9BQUFDZXJ0aWZpY2F0ZVNlcnZpY2VzLmNybDANBgkqhkiG9w0BAQUF
+AAOCAQEACFb8AvCb6P+k+tZ7xkSAzk/ExfYAWMymtrwUSWgEdujm7l3sAg9g1o1Q
+GE8mTgHj5rCl7r+8dFRBv/38ErjHT1r0iWAFf2C3BUrz9vHCv8S5dIa2LX1rzNLz
+Rt0vxuBqw8M0Ayx9lt1awg6nCpnBBYurDC/zXDrPbDdVCYfeU0BsWO/8tqtlbgT2
+G9w84FoVxp7Z8VlIMCFlA2zs6SFz7JsDoeA3raAVGI/6ugLOpyypEBMs1OUIJqsi
+l2D4kF501KKaU73yqWjgom7C12yxow+ev+to51byrvLjKzg6CYG1a4XXvi3tPxq3
+smPi9WIsgtRqAEFQ8TmDn5XpNpaYbg==
+-----END CERTIFICATE-----
+EOF
 
 # Add Self-Signed to fullchain as required by certificate-manager
+CHAIN_ROOT=/root/.acme.sh/$CERTNAME/fullchain-root.cer
+cat $CHAIN $ROOT > $CHAIN_ROOT
 
-curl -s https://letsencrypt.org/certs/isrgrootx1.pem > ${CHAIN_WITH_ROOT}
-cat ${CHAIN} >> /root/.acme.sh/$CERTNAME/fullchainwithroot.cer
+CA_ROOT=/root/.acme.sh/$CERTNAME/ca-root.cer
+cat $CA $ROOT > $CA_ROOT
+
 
 # We delay briefly between account and password, as it's trying to open /dev/tty
 # which has the potential to lose characters. To be on the safe side, we sleep
@@ -81,7 +114,7 @@ cat ${CHAIN} >> /root/.acme.sh/$CERTNAME/fullchainwithroot.cer
   sleep 1
   printf '2\n'
   sleep 1
-  printf '%s\n%s\n%s\ny\n\n' "$CERT" "$KEY" "$CHAIN_WITH_ROOT"
+  printf '%s\n%s\n%s\ny\n\n' "$CHAIN_ROOT" "$KEY" "$CA_ROOT"
 ) | setsid /usr/lib/vmware-vmca/bin/certificate-manager
 
 # 'setsid' detatches certman from /dev/tty, so it's forced to use stdin.
